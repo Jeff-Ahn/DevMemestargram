0cb55ec0b53d91df18967ffb0a779d7084f9a91b
Hyeonguk Ahn <ahu8867@khu.ac.kr>
2021-05-29 00:58:36 +0900

    feat: file s3에 업로드시 파일명 변경

--- server/meme/models.py
+++ server/meme/models.py
@@ -1,34 +1,13 @@
 from django.db import models
 from tag.models import Tag
 import uuid
-import datetime, os
 from config.asset_storage import ImageStorage
-
-
-def set_filename_format(now, filename):
-    return "devmemestargram-{date}-{microsecond}{extension}".format(
-        date=str(now.date()),
-        microsecond=now.microsecond,
-        extension=os.path.splitext(filename)[1],
-    )
-
-
-def directory_path(filename):
-    now = datetime.datetime.now()
-
-    path = "images/{year}/{month}/{day}/{filename}".format(
-        year=now.year,
-        month=now.month,
-        day=now.day,
-        filename=set_filename_format(now, filename),
-    )
-
-    return path
+from .utils import upload_image
 
 
 class Meme(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    image = models.ImageField(storage=ImageStorage)
+    image = models.ImageField(storage=ImageStorage, upload_to=upload_image)
     recommended = models.IntegerField(default=0)
     tags = models.ManyToManyField(Tag)
     description = models.CharField(max_length=255)
--- server/meme/utils.py
+++ server/meme/utils.py
@@ -6,4 +6,4 @@ from django.utils.timezone import now
 def upload_image(instance, filename):
     filename_base, filename_ext = os.path.splitext(filename)
 
-    return "%s" % (now().strftime("%Y%m%d") + "_" + str(uuid.uuid4()))
+    return "%s" % (now().strftime("%Y%m%d") + "_" + str(uuid.uuid4()) + filename_ext)
