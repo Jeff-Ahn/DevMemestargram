9ce1706a0d44daa34cbe74d5ade816c9ad2e9129
Hyeonguk Ahn <ahu8867@khu.ac.kr>
2021-06-01 00:21:48 +0900

    feat: meme create시 작성자를 request보낸 사용자로 확정

--- server/meme/serializers.py
+++ server/meme/serializers.py
@@ -6,14 +6,19 @@ from .utils import delete_image
 class MemeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Meme
-        fields = (
-            "id",
-            "image",
-            "recommended",
-            "tags",
-            "description",
-            "created_at",
-        )
+        fields = "__all__"
+
+    def create(self, validated_data):
+        owner = self.context["request"].user
+        tags = validated_data["tags"]
+        new_tags = tags
+
+        """
+        TODO: 없는 Tag 생성 후, new_tags 추가 후 전달
+        """
+        validated_data.update(owner=owner)
+        validated_data.update(tags=new_tags)
+        return super().create(validated_data)
 
     def update(self, instance, validated_data):
         image = instance.image
