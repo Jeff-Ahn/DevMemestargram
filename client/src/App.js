a96e33b13edd23d2b2249cd3dde0c109e67897c2
Hyeonguk Ahn <ahu8867@khu.ac.kr>
2021-06-06 02:49:30 +0900

    refact: provier(mantine, react-query) 구조 변경

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,15 @@
 import React from 'react';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { createGlobalStyle } from 'styled-components';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { MantineProvider } from '@mantine/core';
 import HomePage from './pages/HomePage';
-import { palette } from './lib/styles/palette';
 import Callback from './pages/Callback';
-
-const GlobalStyle = createGlobalStyle`
-  html,
-  body,
-  #root {
-    margin: 0;
-    padding: 0;
-    height: 100%;
-    background: ${palette.gray0};
-  }
-  html {
-    box-sizing: border-box;
-    * {
-      box-sizing: inherit;
-    }
-  }
-`;
-
-const queryClient = new QueryClient();
 
 function App() {
   return (
     <BrowserRouter>
-      <GlobalStyle />
-      <QueryClientProvider client={queryClient}>
-        <MantineProvider
-          theme={{
-            fontFamily: 'sans-serif',
-            primaryColor: 'teal',
-          }}
-        >
-          <Switch>
-            <Route path="/" exact component={HomePage} />
-            <Route path="/callback" component={Callback} />
-          </Switch>
-        </MantineProvider>
-      </QueryClientProvider>
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/callback" component={Callback} />
+      </Switch>
     </BrowserRouter>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -2,17 +2,50 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { createGlobalStyle } from 'styled-components';
+import { MantineProvider } from '@mantine/core';
 import App from './App';
 import rootReducer from './reducers';
+import { palette } from './lib/styles/palette';
 
 const store = configureStore({
   reducer: rootReducer,
 });
 
+const GlobalStyle = createGlobalStyle`
+  html,
+  body,
+  #root {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    background: ${palette.gray0};
+  }
+  html {
+    box-sizing: border-box;
+    * {
+      box-sizing: inherit;
+    }
+  }
+`;
+
+const queryClient = new QueryClient();
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <GlobalStyle />
+      <QueryClientProvider client={queryClient}>
+        <MantineProvider
+          theme={{
+            fontFamily: 'sans-serif',
+            primaryColor: 'teal',
+          }}
+        >
+          <App />
+        </MantineProvider>
+      </QueryClientProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
