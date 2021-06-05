08314c7c23e0e5ef836b6c58146f4224de1de0c2
Hyeonguk Ahn <ahu8867@khu.ac.kr>
2021-06-05 16:28:06 +0900

    chore: 주석, 공백 제거

--- client/src/hooks/useUser.js
+++ client/src/hooks/useUser.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import { useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setUser } from '../features/user/userSlice';
@@ -7,6 +6,7 @@ function useUser() {
   const user = useSelector(state => state.user);
   const dispatch = useDispatch();
   const setUserData = useCallback(data => dispatch(setUser(data)), [dispatch]);
+
   return [user, setUserData];
 }
 
