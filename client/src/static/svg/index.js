6d6be02ec0b38422652eb87be0f619b05bf15d44
Hyeonguk Ahn <ahu8867@khu.ac.kr>
2021-06-19 00:36:37 +0900

    chore: 필요한 icon svg 파일 추가

--- client/src/static/svg/index.js
+++ client/src/static/svg/index.js
@@ -1,3 +1,6 @@
 export { ReactComponent as SearchIcon } from './icon-search.svg';
 export { ReactComponent as GithubIcon } from './icon-github.svg';
 export { ReactComponent as ExternalLinkIcon } from './icon-external-link.svg';
+export { ReactComponent as CheckIcon } from './icon-check.svg';
+export { ReactComponent as StarIcon } from './icon-star.svg';
+export { ReactComponent as StarFilledIcon } from './icon-star-filled.svg';
